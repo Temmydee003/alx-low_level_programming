d55d93d4f954e69c7ceb9056f9250de28de315e3
Temmydee003 <ekunadyo.temidotunezekiel@gamil.com>
2022-03-11 00:20:28 +0100

    C-Language Basics

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ *
+ *Return: always 0 (success)
+ */
+int main(void)
+{
+	int=i;
+	double=d;
+	char=c;
+
+	printf("The size of an int is: %lu.\n", (unsigned long)sizeof(i));
+	printf("The size of an double is: %lu.\n", (unsigned long)sizeof(d));
+	printf("The size of an c is: %lu.\n", (unsigned long)sizeof(c));
+	return(0);
+}
