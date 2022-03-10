2619da71e8e66ed2ea1bbafefb175e0aec905411
Temmydee003 <ekunadyo.temidotunezekiel@gamil.com>
2022-03-11 00:37:16 +0100

    C-Language Basics

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- *main- Entry point
+ *main - Entry point
  *
  *return: always 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-		return (0);
+        return (0);
 }
