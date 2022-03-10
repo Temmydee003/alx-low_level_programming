37fb1b9ba6426cbad48f27faf4b24eca747e1e5c
Temmydee003 <ekunadyo.temidotunezekiel@gamil.com>
2022-03-11 00:30:19 +0100

    C-Langauge Basics

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,11 @@
 
 /**
  *main- Entry point
+ *
  *return: always 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-		return(0);
+		return (0);
 }
