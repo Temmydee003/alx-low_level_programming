14f252b64c17da934fef403f6fc952a6d8c01d3d
Temmydee003 <ekunadyo.temidotunezekiel@gamil.com>
2022-03-11 23:21:42 +0100

    fixed

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * return: always 0 (success)
+ */
+int main(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
