08e8f7d0f5a359ba1c796c3be034fc5d52eff6f4
Temmydee003 <ekunadyo.temidotunezekiel@gamil.com>
2022-03-11 23:26:17 +0100

    fixed

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,7 +4,7 @@
 
 /**
  * main - entry point
- * return: always 0 (success)
+ * return: always 0 (Success)
  */
 int main(void)
 {
@@ -12,7 +12,7 @@ int main(void)
 
 	while (a < 10)
 	{
-		putchar(a);
+		putchar("%d", a);
 	}
 	putchar('\n');
 	return (0);
