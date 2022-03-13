a3fe555bdc734c6698daf47159edfe620534440f
Temmydee003 <ekunadyo.temidotunezekiel@gamil.com>
2022-03-13 21:32:03 +0100

    C-Language Basics

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - entry point
- *
- *return: always 1 (success)
+ * main - Entry point
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	write(2, "and and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	write(2, "and and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return  (1);
 }
