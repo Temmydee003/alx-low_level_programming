e3954b7c5c6c13ce67767183baf00511ed336edc
Temmydee003 <ekunadyo.temidotunezekiel@gamil.com>
2022-03-14 01:33:07 +0100

    combination of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,9 +10,9 @@ int main(void)
 	int a;
 	int b;
 
-	for (a = '0'; a <= '9'; a++)
+	for (a = '0'; a <= '8'; a++)
 	{
-		for (b = '0'; b <= '9'; b++)
+		for (b = a + 1; b <= '9'; b++)
 		{
 			putchar(a);
 			putchar(b);
