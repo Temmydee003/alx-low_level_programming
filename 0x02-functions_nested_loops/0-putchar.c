899b18b66c63546ba98662efb25f9bc5de455699
Temmydee003 <ekunadyo.temidotunezekiel@gamil.com>
2022-03-15 23:57:40 +0100

    print a word

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+int e = 5
+{
+	if (e > 10)
+	{
+		printf("man\n");
+	}
+	else
+		printf("_putchar\n");
+	return 0;
+}
