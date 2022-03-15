69beea47addcffbdb3c80a639f05ece19d9c2411
Temmydee003 <ekunadyo.temidotunezekiel@gamil.com>
2022-03-16 00:09:03 +0100

    print lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int e;
+
+	for (e = 'a', e <= 'z' e++);
+	{
+		_putchar(e);
+	}
+		_putchar("\n");
+	return 0;
+}
